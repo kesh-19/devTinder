24b7a635d28c4acf4fb33b59b8e6dbca4e81bf61
Shreyas Kelshikar <shreyaskelshikar19@gmail.com>
2025-07-22 22:06:55 +0530

    Implement middleware for "/fly/:flyId" route to handle requests and calculate sum

--- src/app.js
+++ src/app.js
@@ -4,9 +4,30 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.get(
+  "/fly/:flyId",
+  (req, res, next) => {
+    console.log("Hello World 1");
+    res.sum = 1;
+    next();
+  },
+  (req, res, next) => {
+    console.log("Hello World 2");
+    res.sum++;
+    next();
+  },
+  (req, res, next) => {
+    console.log("Hello World 3");
+    res.sum++;
+    next();
+  },
+  (req, res, next) => {
+    console.log("Hello World 4");
+    res.sum++;
+    res.send(res.sum.toString());
+    // next();
+  }
+);
 app.get("/test", (req, res) => {
   res.send("Hello Test");
 });
