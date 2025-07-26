d8a0df6c75cacf09076c2e7547d5bdf11831c209
Shreyas Kelshikar <shreyaskelshikar19@gmail.com>
2025-07-26 16:21:31 +0530

    pusing dummy user to db

--- src/app.js
+++ src/app.js
@@ -1,24 +1,37 @@
 const express = require("express");
-const connectDB = require("./config/database");
 const app = express();
+const connectDB = require("./config/database");
+
+// Import models
+const UserModel = require("./models/user");
 
 // Initialize Express app
 const runServer = () => {
-  express().listen(3000, () => {
+  app.listen(3000, () => {
     console.log("Server is running on port 3000");
   });
 };
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
   // Logic to create a new user
 
-  const dummyUser = {
+  const userObj = {
     firstName: "John",
     lastName: "Doe",
     emailId: "john.doe@gmail.com",
     password: "password123",
     age: 27,
   };
+
+  const user = new UserModel(userObj);
+
+  try {
+    await user.save();
+    res.send("User created successfully");
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 connectDB()
